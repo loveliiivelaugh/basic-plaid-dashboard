0ece333eee09088f6644103f06bc0af519840b6f
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2022-05-10 20:47:19 -0500

    Updated plaid config for new version

--- utils/plaid.js
+++ utils/plaid.js
@@ -1,16 +1,32 @@
 //? https://plaid.com/docs/quickstart/#quickstart-setup
-const plaid = require('plaid');
+// const plaid = require('plaid');
 require('dotenv').config();
 
-// Initialize the Plaid client
-// Find your API keys in the Dashboard (https://dashboard.plaid.com/account/keys)
-const plaidClient = new plaid.Client({
-  clientID: process.env.PLAID_CLIENT_ID,
-  secret: process.env.PLAID_SECRET,
-  env: plaid.environments.sandbox,
-  options: {
-    version: '2020-09-14',
+const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments[process.env.PLAID_ENV],
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+      'PLAID-SECRET': process.env.PLAID_SECRET,
+    },
   },
 });
 
-module.exports = plaidClient;
+const client = new PlaidApi(configuration);
+
+
+
+// // Initialize the Plaid client
+// // Find your API keys in the Dashboard (https://dashboard.plaid.com/account/keys)
+// const plaidClient = new plaid.Client({
+//   clientID: process.env.PLAID_CLIENT_ID,
+//   secret: process.env.PLAID_SECRET,
+//   env: plaid.environments.sandbox,
+//   options: {
+//     version: '2020-09-14',
+//   },
+// });
+
+module.exports = client;
