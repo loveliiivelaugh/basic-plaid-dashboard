2c6dba0ddc48dbfe75ddc1aafe533f6698ba500f
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2022-05-20 11:42:56 -0500

    rm file

--- frontend/src/components/PlaidVisuals.jsx
+++ frontend/src/components/PlaidVisuals.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import { Card, Container, Grid, Typography } from  '@mui/material';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, Container, Grid, Typography } from  '@mui/material';
 import DataGridDemo from './MuiDatagrid';
 import Barchart from './recharts/BarChart';
 import Piechart from './recharts/Piechart';
 
-export default function PlaidVisuals({ data: { accounts, transactions }, loading }) {
+export default function PlaidVisuals({ loading, data = false }) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!data) navigate('/');
+  }, []);
+  const { accounts, transactions } = data;
   console.log({ accounts, transactions, loading });
   const jsx = (
-    <Container maxWidth={false}>
+    <Container maxWidth="xl">
       {accounts && (
-        <>
+        <Paper elevation={12} sx={{ p: 2, m: 2 }}>
+          <Typography variant="h4" gutterBottom>
+            Accounts
+          </Typography>
           <Grid container>
             <Grid item xs={12} sm={4}>
-              {/* <Piechart data={accounts} /> */}
+              <Piechart data={accounts} />
             </Grid>
             <Grid item xs={12} sm={8}>
               <Barchart data={accounts} />
@@ -21,25 +30,28 @@ export default function PlaidVisuals({ data: { accounts, transactions }, loading
           <Grid container>
             <DataGridDemo data={accounts} />
           </Grid>
-        </>
+        </Paper>
+      )}
+      {transactions && (
+        <Paper elevation={12} sx={{ p: 2, m: 2 }}>
+          <Typography variant="h4" gutterBottom>
+            Transactions
+          </Typography>
+          <Grid container>
+            <Grid item xs={12} sm={4}>
+              <Piechart data={transactions} />
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <Barchart data={transactions} />
+            </Grid>
+          </Grid>
+          <Grid container>
+            <DataGridDemo data={transactions} />
+          </Grid>
+        </Paper>
       )}
     </Container>
   )
 
   return jsx;
 };
-// {transactions && (
-//   <>
-//     <Grid container>
-//       <Grid item xs={12} sm={4}>
-//         {/* <Piechart data={transactions} /> */}
-//       </Grid>
-//       <Grid item xs={12} sm={8}>
-//         <Barchart data={transactions} />
-//       </Grid>
-//     </Grid>
-//     <Grid container>
-//       <DataGridDemo data={transactions} />
-//     </Grid>
-//   </>
-// )}
