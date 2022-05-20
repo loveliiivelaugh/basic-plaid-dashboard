60ab92e3331a9612658f73d2bd80c4ff489319e7
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2022-05-20 12:28:38 -0500

    Added missing prod env scripts

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.use(express.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
 
 app.use(cors());
-
+//check dev or production environment
+if ( process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+}
+// 🔀  server routes 🔀 
 app.use('/', require('./router'));
 
 app.listen(port, () => console.info(`Example app listening at http://localhost:${port}`));
