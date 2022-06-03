796a12cb1983a18caaf463f189f2e68fd30073a5
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2022-06-03 13:02:17 -0500

    Fixed client side api for prod

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,16 +1,10 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8080';
-// const headers = {
-//   'Content-Type': 'application/json',
-//   'Access-Control-Allow-Origin': '*',
-// };
-
 const api = {
-  createLinkToken: async () => axios.post(`${url}/api/create_link_token`),
-  exchangePublicToken: async (public_token, user_id) => axios.post(`${url}/api/exchange_public_token`, { public_token, user_id }),
-  getAccounts: async () => axios.get(`${url}/api/accounts`),
-  getTransactions: async () => axios.get(`${url}/api/transactions`),
+  createLinkToken: async () => axios.post(`/api/create_link_token`),
+  exchangePublicToken: async (public_token, user_id) => axios.post(`/api/exchange_public_token`, { public_token, user_id }),
+  getAccounts: async () => axios.get(`/api/accounts`),
+  getTransactions: async () => axios.get(`/api/transactions`),
 };
 
 export default api;
